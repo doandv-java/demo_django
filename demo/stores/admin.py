177dc8abd079a906e7b538cc0c994fd7b341c6c6
Doan <doandv@ominext.com>
2020-04-28 18:20:03 +0700

    create admin category admin

--- demo/stores/admin.py
+++ demo/stores/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Store, Book
+
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    # fieldsets = [
+    #     ('Name', {'fields': ['name']}),
+    #     ('Name Stores', {'fields': ['store_names']})
+    # ]
+    list_display = ('name', 'name_store')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book)
+admin.site.register(Store)
--- demo/stores/models.py
+++ demo/stores/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.TextField(max_length=200)
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    store_names = []
+
+    def __str__(self):
+        return self.name
+
+    def name_store(self):
+        return self.store.name
+
+    def store_names(self):
+        for name in self.store:
+            self.store_names.append(name)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    cost = models.FloatField(default=0.0)
+    page_number = models.IntegerField(default=0)
